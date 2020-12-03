6159ad37becd110641e2984c2dc711996c11254c
sery270 <sery270@naver.com>
2020-12-03 16:55:06 +0900

    [ADD] UIColorExtension added

---
+++ HAPDONG_Seminar_Project/HAPDONG_Seminar_Project/Resource/Extension/UIColorExtension.swift
@@ -0,0 +1,72 @@
+//
+//  UIColorExtension.swift
+//  HAPDONG_Seminar_Project
+//
+//  Created by 박세란 on 2020/12/03.
+//
+
+import Foundation
+
+extension UIColor {
+
+  @nonobjc class var veryLightPink: UIColor {
+    return UIColor(white: 238.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPinkTwo: UIColor {
+    return UIColor(white: 220.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var darkPeach: UIColor {
+    return UIColor(red: 221.0 / 255.0, green: 110.0 / 255.0, blue: 90.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var greyishBrown: UIColor {
+    return UIColor(white: 83.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var cloudyBlue: UIColor {
+    return UIColor(red: 171.0 / 255.0, green: 214.0 / 255.0, blue: 211.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var black: UIColor {
+    return UIColor(white: 51.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownGrey: UIColor {
+    return UIColor(white: 130.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var greyishBrownTwo: UIColor {
+    return UIColor(white: 62.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var pinkishGrey: UIColor {
+    return UIColor(red: 175.0 / 255.0, green: 174.0 / 255.0, blue: 174.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var blackTwo: UIColor {
+    return UIColor(white: 36.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var white: UIColor {
+    return UIColor(white: 247.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var brownGreyTwo: UIColor {
+    return UIColor(white: 137.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var veryLightPinkThree: UIColor {
+    return UIColor(white: 196.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var whiteTwo: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var blackThree: UIColor {
+    return UIColor(white: 0.0, alpha: 1.0)
+  }
+
+}
